bbce96c7d9b3d284236f5d4ded09e4c5f6aed89e
Zach Leatherman <zachleatherman@gmail.com>
2018-04-04 08:53:34 -0500

    Support namespacing with Eleventy v0.3.1

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const plainHighlighter = require("./src/syntaxHighlightPlain");
 const prismjsHighlighter = require("./src/syntaxHighlightPrism");
 
-module.exports = function(eleventyConfig) {
-  // compatibility with existing {% highlight js %} and others
-  eleventyConfig.addLiquidTag("highlight", prismjsHighlighter);
+module.exports = function(eleventyConfig, pluginNamespace) {
+  eleventyConfig.namespace(pluginNamespace, () => {
+    // compatibility with existing {% highlight js %} and others
+    eleventyConfig.addLiquidTag("highlight", prismjsHighlighter);
 
-  eleventyConfig.addLiquidTag("highlight-plain", plainHighlighter);
+    eleventyConfig.addLiquidTag("highlight-plain", plainHighlighter);
+  });
 };
