a355aa2889254d1c5f07fd308c92f510d53929e5
Zach Leatherman <zachleatherman@gmail.com>
2019-12-30 22:03:28 -0600

    Stray console log in demo

--- demo/eleventy-config.js
+++ demo/eleventy-config.js
@@ -1,6 +1,5 @@
 const syntaxHighlight = require("../.eleventy.js");
 
-console.log( syntaxHighlight.pairedShortcode );
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight, {
     // alwaysWrapLineHighlights: true
