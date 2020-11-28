8323d7b7be94808b882b9030320fcdcc05f766e3
Zach Leatherman <zachleatherman@gmail.com>
2020-11-27 21:50:13 -0600

    Better test for #40

--- src/LiquidHighlightTag.js
+++ src/LiquidHighlightTag.js
@@ -6,41 +6,39 @@ class LiquidHighlightTag {
   }
 
   getObject(options = {}) {
-    let ret = function(highlighter) {
-      return {
-        parse: function(tagToken, remainTokens) {
-          let split = tagToken.args.split(" ");
+    return {
+      parse: function(tagToken, remainTokens) {
+        let split = tagToken.args.split(" ");
 
-          this.language = split.shift();
-          this.highlightLines = split.join(" ");
+        this.language = split.shift();
+        this.highlightLines = split.join(" ");
 
-          this.tokens = [];
+        this.tokens = [];
 
-          var stream = highlighter.liquidEngine.parser.parseStream(remainTokens);
+        var stream = this.liquid.parser.parseStream(remainTokens);
 
-          stream
-            .on("token", token => {
-              if (token.name === "endhighlight") {
-                stream.stop();
-              } else {
-                this.tokens.push(token);
-              }
-            })
-            .on("end", x => {
-              throw new Error(`tag ${tagToken.getText()} not closed`);
-            });
+        stream
+          .on("token", token => {
+            if (token.name === "endhighlight") {
+              stream.stop();
+            } else {
+              this.tokens.push(token);
+            }
+          })
+          .on("end", x => {
+            throw new Error(`tag ${tagToken.getText()} not closed`);
+          });
 
-          stream.start();
-        },
-        render: function(scope, hash) {
-          let tokens = this.tokens.map(token => token.raw || token.input.substring(token.begin, token.end));
-          let tokenStr = tokens.join("").trim();
-          return Promise.resolve(HighlightPairedShortcode(tokenStr, this.language, this.highlightLines, options));
-        }
-      };
+        stream.start();
+      },
+      render: function(scope, hash) {
+        let tokens = this.tokens.map(token => {
+          return token.raw || token.input.substring(token.begin, token.end)
+        });
+        let tokenStr = tokens.join("").trim();
+        return Promise.resolve(HighlightPairedShortcode(tokenStr, this.language, this.highlightLines, options));
+      }
     };
-
-    return ret(this);
   }
 }
 
---
+++ test/LiquidHighlightTagTest.js
@@ -0,0 +1,25 @@
+const test = require("ava");
+const { Liquid } = require('liquidjs');
+const LiquidHighlightTag = require("../src/LiquidHighlightTag");
+
+async function renderLiquid(str, data = {}, engine = null) {
+	if(!engine) {
+		engine = new Liquid();
+	}
+
+	let result = await engine.parseAndRender(str, data);
+	return result;
+}
+
+test("Test Render", async t => {
+	t.is("Hi Zach", await renderLiquid("Hi {{name}}", {name: "Zach"}));
+});
+
+test("Test Highlight Tag Render", async t => {
+  let engine = new Liquid();
+  let tag = new LiquidHighlightTag();
+  engine.registerTag("highlight", tag.getObject());
+
+  let rendered = await renderLiquid("{% highlight js %}var test;{% endhighlight %}", {}, engine);
+	t.is(`<pre class="language-js"><code class="language-js"><span class="token keyword">var</span> test<span class="token punctuation">;</span></code></pre>`, rendered);
+});
