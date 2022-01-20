6bc55ade33611cc81a31b7ed03ca5de406ff0404
Zach Leatherman <zachleatherman@gmail.com>
2022-01-20 14:42:30 -0600

    Fix: Aliases weren’t resolving correctly for diff languages

--- src/PrismLoader.js
+++ src/PrismLoader.js
@@ -6,20 +6,34 @@ PrismLoader.silent = true;
 const PrismAlias = require("./PrismNormalizeAlias");
 
 module.exports = function(language) {
-  if(language.startsWith("diff-") && !Prism.languages.diff) {
+  let diffRemovedRawName = language;
+  if(language.startsWith("diff-")) {
+    diffRemovedRawName = language.substr("diff-".length);
+  }
+  let diffRemovedNormalizedName = PrismAlias(diffRemovedRawName);
+
+  if(!Prism.languages[ diffRemovedNormalizedName ]) {
+    PrismLoader(diffRemovedNormalizedName);
+  }
+  if(!Prism.languages[ diffRemovedNormalizedName ]) {
+    throw new Error(`"${language}" is not a valid Prism.js language for eleventy-plugin-syntaxhighlight`);
+  }
+  if(!language.startsWith("diff-")) {
+    return Prism.languages[ diffRemovedNormalizedName ];
+  }
+
+  let fullLanguageName = `diff-${diffRemovedNormalizedName}`;
+
+  if(!Prism.languages.diff) {
     PrismLoader("diff");
     // Bundled Plugin
     require("prismjs/plugins/diff-highlight/prism-diff-highlight");
-
-    return Prism.languages.diff;
   }
 
-  let normalizedLanguage = PrismAlias(language);
-  if(!Prism.languages[ normalizedLanguage ]) {
-    PrismLoader(normalizedLanguage);
-  }
-  if(!Prism.languages[normalizedLanguage]) {
-    throw new Error(`"${language}" is not a valid Prism.js language for eleventy-plugin-syntaxhighlight`);
-  }
-  return Prism.languages[ normalizedLanguage ];
+  // Store into with aliased keys
+  //   ts -> diff-typescript
+  //   js -> diff-javascript
+  Prism.languages[ fullLanguageName ] = Prism.languages.diff;
+
+  return Prism.languages[ fullLanguageName ];
 };
