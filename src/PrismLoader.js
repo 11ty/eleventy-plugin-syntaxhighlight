cea95e9da7feef4cf03e7797fe534aee2b3e368f
Zach Leatherman <zachleatherman@gmail.com>
2022-01-20 12:23:58 -0600

    Work with `diff-` languages for the diff-highlight plugin.

--- src/PrismLoader.js
+++ src/PrismLoader.js
@@ -3,6 +3,11 @@ const PrismLoader = require("prismjs/components/index.js");
 const PrismAlias = require("./PrismNormalizeAlias");
 
 module.exports = function(language) {
+  if(language.startsWith("diff-") && !Prism.languages.diff) {
+    PrismLoader("diff");
+    return Prism.languages.diff;
+  }
+
   let normalizedLanguage = PrismAlias(language);
   if(!Prism.languages[ normalizedLanguage ]) {
     PrismLoader(normalizedLanguage);
